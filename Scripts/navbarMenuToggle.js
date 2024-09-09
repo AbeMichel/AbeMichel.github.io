67e1dedbf93d1491beaadbd8f1ed7097998dbc55
AbeMic <122504959+AbeMic@users.noreply.github.com>
2024-09-09 16:54:55 -0500

    Added navbar support for smaller screens

---
+++ Scripts/navbarMenuToggle.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const menuBtn = document.getElementById('nav-menu-btn');
+    const menu = document.getElementById('nav-links');
+
+    menuBtn.addEventListener('click', function(){
+        menu.classList.toggle('active');
+    });
+});
