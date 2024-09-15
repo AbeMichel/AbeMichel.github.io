391f567b8c878e020b9b3b9cd40d0eecaa51e3bc
AbeMichel <abemicheljob@gmail.com>
2024-09-15 12:25:04 -0500

    deleted large video files

---
+++ Scripts/imagecarousel.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // create a list of all images in the carousel
+    // select 1 as the front image
+    // when prev or next is clicked move the current index
+    // have the other images rotate positions
+    // update z-indexes
+});
