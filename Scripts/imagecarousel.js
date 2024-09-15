c1c5d198a78e53258286c2d6d09d4e304008c920
AbeMichel <abemicheljob@gmail.com>
2024-09-15 17:07:29 -0500

    Fixed display of images in metal sign page, added a project picture to MQTT script gen, and made overflow-x auto for project photos

--- Scripts/imagecarousel.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    // create a list of all images in the carousel
-    // select 1 as the front image
-    // when prev or next is clicked move the current index
-    // have the other images rotate positions
-    // update z-indexes
-});
